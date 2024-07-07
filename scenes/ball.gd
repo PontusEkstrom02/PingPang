48171635ab0527b12649b842f3305b708ccf5aec
Tussee098 <Tusse098@hotmail.com>
2024-07-07 15:06:20 +0200

    beginning of ball movement

---
+++ scenes/ball.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+var win_size : Vector2
+var speed : int = 100
+var dir : Vector2
+
+func _ready():
+	dir = Vector2
+	
+
+func _movement_process(delta):
+	move_and_collide(dir * speed * delta)
