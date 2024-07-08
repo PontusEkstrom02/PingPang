e9f0c9fb39245339f2ebf1c8c5a8d33e24cfc7ef
Tussee098 <Tusse098@hotmail.com>
2024-07-08 15:32:04 +0200

    Layer & Mask Names

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 var screen_size : Vector2
 var speed : int = 100
 var dir : Vector2
-var collision_data : KinematicCollision2D
+var collision_data : KinematicCollision2D = null
 
 func _ready():
 	dir = Vector2(0, 1)
@@ -12,8 +12,8 @@ func _ready():
 func _physics_process(delta):
 	
 	collision_data = move_and_collide(dir * speed * delta)
-	if collision_data:
-		velocity.y += randf_range(-20, 20) #rand_range(-20, 20) for Godot 3
+	if collision_data != null:
+		print("dsffsdsdfsdf")
 		velocity = velocity.bounce(collision_data.get_normal())
-		print("sdaasd")
+		
 
