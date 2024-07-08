fb55f2edac3630d2418b2d65f58b3e92e8c5a819
Tussee098 <Tusse098@hotmail.com>
2024-07-08 15:05:08 +0200

    Binds fixed

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,15 +1,19 @@
 extends CharacterBody2D
 
-var win_size : Vector2
+var screen_size : Vector2
 var speed : int = 100
 var dir : Vector2
+var collision_data : KinematicCollision2D
 
 func _ready():
-	dir = Vector2(0,-1)
+	dir = Vector2(0, 1)
+	screen_size = get_viewport_rect().size
+
+func _physics_process(delta):
 	
+	collision_data = move_and_collide(dir * speed * delta)
+	if collision_data:
+		velocity.y += randf_range(-20, 20) #rand_range(-20, 20) for Godot 3
+		velocity = velocity.bounce(collision_data.get_normal())
+		print("sdaasd")
 
-func _process(delta):
-	_movement_process(delta)
-
-func _movement_process(delta):
-	move_and_collide(dir * speed * delta)
