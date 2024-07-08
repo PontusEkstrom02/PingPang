daa3a4bcf6c550e50aa9cca8af8867529f37c4f8
Tussee098 <Tusse098@hotmail.com>
2024-07-08 14:49:47 +0200

    Ball moving

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -5,8 +5,11 @@ var speed : int = 100
 var dir : Vector2
 
 func _ready():
-	dir = Vector2(0,0)
+	dir = Vector2(0,-1)
 	
+
+func _process(delta):
+	_movement_process(delta)
 
 func _movement_process(delta):
 	move_and_collide(dir * speed * delta)
