0f0d03f7f378e0c293444dcdf8fd7a9ba632a53a
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2024-07-08 23:33:03 +0200

    Fixed gameArea

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,19 +1,16 @@
 extends CharacterBody2D
 
-var screen_size : Vector2
-var speed : int = 100
-var dir : Vector2
+var speed : int = 10000 # Fix speed (high for testing)
+var dir : Vector2 = Vector2(1, 1).normalized()
 var collision_data : KinematicCollision2D = null
 
 func _ready():
-	dir = Vector2(0, 1)
-	screen_size = get_viewport_rect().size
+	pass
 
 func _physics_process(delta):
-	
 	collision_data = move_and_collide(dir * speed * delta)
-	if collision_data != null:
-		print("dsffsdsdfsdf")
-		velocity = velocity.bounce(collision_data.get_normal())
-		
+	
+	if collision_data:
+		dir = dir.bounce(collision_data.get_normal()).normalized()
+
 
